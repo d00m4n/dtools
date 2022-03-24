b1541e6c22100db00970b6ed1434d853b821c4b1
d00m4n <github@doomans.com>
2022-03-24 19:48:41 +0100

    fix init

--- __init__.py
+++ __init__.py
@@ -1,1 +1,6 @@
-import dtools
+from .dtools import epochToHuman
+from .dtools import wait
+from .dtools import showError
+from .dtools import downloadDataImage
+from .dtools import getCrc 
+from .dtools import Folder
